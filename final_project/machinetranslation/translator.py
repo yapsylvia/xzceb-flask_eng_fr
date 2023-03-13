4e18d052f21b60576b9e5631cdf0cdd1d3d4f25b
yapsylvia <yapsylvia@yahoo.com>
2023-03-13 10:50:50 -0400

    temporary changes2

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,3 +8,26 @@ load_dotenv()
 
 apikey = os.environ['apikey']
 url = os.environ['url']
+
+
+authenticator = IAMAuthenticator('{apikey}')
+language_translator = LanguageTranslatorV3(
+    version='{version}',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url('{url}')
+
+def englishToFrench(englishText):
+    #write the code here
+    frenchText = language_translator.translate(
+    text=englishText,
+    model_id='en-fr').get_result()
+    return frenchText
+
+def frenchToEnglish(frenchText):
+    #write the code here
+    englishText = language_translator.translate(
+    text=frenchText,
+    model_id='fr-en').get_result()
+    return englishText
