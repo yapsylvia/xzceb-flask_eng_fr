890831c8a34e5673ea8d2bc14e0fb660e6d4ac3c
yapsylvia <yapsylvia@yahoo.com>
2023-03-14 14:09:27 +0100

    Modification final Changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -8,6 +8,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+#apikey='ZYSl9JhsoUi8fG4nXmgUJQDKaVl_aNMPVsLxaW5HW3i5'
+#url='https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/c5f57fb1-8090-4c56-81ab-c3d6a0a71c83'
 apikey = os.environ['apikey']
 url = os.environ['url']
 
@@ -22,17 +24,23 @@ language_translator.set_service_url(url)
 def english_to_french(english_text):
     #write the code here
     """ function to translate from english to french"""
-    french_text = language_translator.translate(
-    text=english_text,
-    model_id='en-fr').get_result()
-    print(json.dumps(french_text, indent=2, ensure_ascii=False))
-    return french_text
+    if english_text is not None: 
+        french_text = language_translator.translate(
+        text=english_text,
+        model_id='en-fr').get_result()
+        print(json.dumps(french_text, indent=2, ensure_ascii=False))
+        return french_text['translations'][0]['translation']
+    
+    return ""
 
 def french_to_english(french_text):
     #write the code here
     """ function to translate from french to english"""
-    english_text = language_translator.translate(
-    text=french_text,
-    model_id='fr-en').get_result()
-    print(json.dumps(english_text, indent=2, ensure_ascii=False))
-    return english_text
+    if french_text is not None:
+        english_text = language_translator.translate(
+        text=french_text,
+        model_id='fr-en').get_result()
+        print(json.dumps(english_text, indent=2, ensure_ascii=False))
+        return english_text['translations'][0]['translation']
+
+    return ""
